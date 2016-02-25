9b88feae957b064b1d6c5ceea458ad399e02ae12
bo <sian.chen@valpo.edu>
2016-02-25 12:52:13 -0500

    changeToColumnChart

--- public/hchart.js
+++ public/hchart.js
@@ -2,138 +2,84 @@
     // Uncomment to style it like Apple Watch
     var opts = {
         chart: {
-            type: 'solidgauge',
-            marginTop: 50
+            type: 'column',
+            options3d: {
+                enabled: true,
+                alpha: 10,
+                beta: 25,
+                depth: 50
+            }
         },
         title: {
-            text: 'Activity',
-            style: {
-                fontSize: '24px'
-            }
+            text: 'Weekly Chat Traffic Statisitcs'
         },
-
-        tooltip: {
-            borderWidth: 0,
-            backgroundColor: 'none',
-            shadow: false,
-            style: {
-                fontSize: '16px'
-            },
-            pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
-            positioner: function(labelWidth, labelHeight) {
-                return {
-                    x: 200 - labelWidth / 2,
-                    y: 180
-                };
-            }
+        subtitle: {
+            text: 'Take a look, did you study this week?'
         },
-
-        pane: {
-            startAngle: 0,
-            endAngle: 360,
-            background: [{ // Track for Move
-                outerRadius: '112%',
-                innerRadius: '88%',
-                backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.3).get(),
-                borderWidth: 0
-            }, { // Track for Exercise
-                outerRadius: '87%',
-                innerRadius: '63%',
-                backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0.3).get(),
-                borderWidth: 0
-            }, { // Track for Stand
-                outerRadius: '62%',
-                innerRadius: '38%',
-                backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[2]).setOpacity(0.3).get(),
-                borderWidth: 0
-            }]
-        },
-
-        yAxis: {
-            min: 0,
-            max: 100,
-            lineWidth: 0,
-            tickPositions: []
-        },
-
         plotOptions: {
-            solidgauge: {
-                borderWidth: '34px',
-                dataLabels: {
-                    enabled: false
-                },
-                linecap: 'round',
-                stickyTracking: false
+            column: {
+                depth: 25
             }
         },
+        xAxis: {
+           // categories: Highcharts.getOptions().lang.shortWeeks
+                       categories: ['Mon', 'Tus', 'Wed', 'Thur', 'Fri','Sat','Sun']
 
+        },
+        yAxis: {
+            title: {
+                text:'message  amount'
+            }
+        },
         series: [{
-            name: 'Message Sum',
-            borderColor: Highcharts.getOptions().colors[0],
-            data: [{
-                color: Highcharts.getOptions().colors[0],
-                radius: '100%',
-                innerRadius: '100%',
-                y: 80
-            }]
-        }, {
-            name: 'Actived members',
-            borderColor: Highcharts.getOptions().colors[1],
-            data: [{
-                color: Highcharts.getOptions().colors[1],
-                radius: '75%',
-                innerRadius: '75%',
-                y: 65
-            }]
-        }, {
-            name: 'TimeSum',
-            borderColor: Highcharts.getOptions().colors[2],
-            data: [{
-                color: Highcharts.getOptions().colors[2],
-                radius: '50%',
-                innerRadius: '50%',
-                y: 50
-            }]
+            name: 'Room1',
+            data: [2, 3, 9, 4, 3, 5, 1]
+        },{
+            name:'Room2',
+            data: [4,2, 1, 4, 1, 5, 1]
+        },{
+            name:'Room3',
+            data: [4,2,2, 4, 1, 5, 3]
         }]
     };
 
     function callback() {
-
-        // Move icon
-        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
-            .attr({
-                'stroke': '#FFCC33',
-                'stroke-linecap': 'round',
-                'stroke-linejoin': 'round',
-                'stroke-width': 2,
-                'zIndex': 10
-            })
-            .translate(190, 26)
-            .add(this.series[2].group);
-
-        // Exercise icon
-        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])//, 'M', 8, -8, 'L', 16, 0, 8, 8])
-            .attr({
-                'stroke': '#FFCC33',
-                'stroke-linecap': 'round',
-                'stroke-linejoin': 'round',
-                'stroke-width': 2,
-                'zIndex': 10
-            })
-            .translate(190, 61)
-            .add(this.series[2].group);
-
-        // Stand icon
-        this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
-            .attr({
-                'stroke': '#FFCC33',
-                'stroke-linecap': 'round',
-                'stroke-linejoin': 'round',
-                'stroke-width': 2,
-                'zIndex': 10
-            })
-            .translate(190, 96)
-            .add(this.series[2].group);
+//
+//        // Move icon
+//        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+//            .attr({
+//                'stroke': '#FFCC33',
+//                'stroke-linecap': 'round',
+//                'stroke-linejoin': 'round',
+//                'stroke-width': 2,
+//                'zIndex': 10
+//            })
+//            .translate(190, 26)
+//            .add(this.series[2].group);
+//
+//        // Exercise icon
+//        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8]) //, 'M', 8, -8, 'L', 16, 0, 8, 8])
+//            .attr({
+//                'stroke': '#FFCC33',
+//                'stroke-linecap': 'round',
+//                'stroke-linejoin': 'round',
+//                'stroke-width': 2,
+//                'zIndex': 10
+//            })
+//            .translate(190, 61)
+//            .add(this.series[2].group);
+//
+//        // Stand icon
+//        this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
+//            .attr({
+//                'stroke': '#FFCC33',
+//                'stroke-linecap': 'round',
+//                'stroke-linejoin': 'round',
+//                'stroke-width': 2,
+//                'zIndex': 10
+//            })
+//            .translate(190, 96)
+//            .add(this.series[2].group);
     };
     angular.module('main')
         .service('myChartUtil', function() {
